860039e09c2049eb49384d8fd48ad73733de1801
NoahSchiro <noahschiro@rocketmail.com>
2022-09-12 14:58:31 -0400

    Java solution for p5

--- project_euler/p5/solution.java
+++ project_euler/p5/solution.java
@@ -1,12 +1,40 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
 class Solution {
 
-	static void solve() {
+	static void solve(int divisor) {
 
+		int ans = 20;
+
+		//While we have not found a number
+		while(true) {
+
+			//For all the divisors
+			for(int i = 1; i <= divisor; i++) {
+
+				//If it fails, break and continue onto the next number
+				if(ans % i != 0) {
+					break;
+				} else {
+
+					//If it made it through all 20 and this is true
+					if(i == 20) {
+						return ans;
+					}
+				}
+			}
+
+			//Increment
+			ans++;
+		}
 	}
 
 	public static void main(String[] args) {
 
-		solve();
+		System.out.println(solve(20));
 
 	}
 };
