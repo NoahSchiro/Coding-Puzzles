ce2d7997e9cb842bffb41e5be4c35ba243380e3d
NoahSchiro <noahschiro@rocketmail.com>
2022-09-19 15:51:19 -0400

    Added java solution to p7

--- project_euler/p7/solution.java
+++ project_euler/p7/solution.java
@@ -1,12 +1,40 @@
+import java.lang.Math;
+
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10001st prime number?
+*/
 class Solution {
 
-	static void solve() {
+	//Check if a number is prime
+	static boolean isPrime(int num) {
 
+		if(num == 2) {return true;}
+
+		if(num % 2 == 0) {return false;}
+
+		for(int i = 3; i <= Math.sqrt(num); i+=2) {
+			if(num % i == 0) {return false;}
+		}
+
+		return true;
 	}
 
 	public static void main(String[] args) {
 
-		solve();
+		//Count up the primes
+		int counter = 0;
+		int index = 2;
+
+		while(counter < 10001) {
+
+			if(isPrime(index)) {
+				System.out.println(index);
+				counter++;
+			}
+
+			index++;
+		}
 
 	}
 };
