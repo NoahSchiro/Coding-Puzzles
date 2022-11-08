e87fe714163eceb7eacd1fad68897f5681c7ec79
NoahSchiro <noahschiro@rocketmail.com>
2022-11-08 08:07:07 -0500

    Added solutions to project euler

--- project_euler/p012/solution.hs
+++ project_euler/p012/solution.hs
@@ -21,9 +21,10 @@ triangleNumbers = scanl1 (+) [1..]
 -- Returns the number of divisors for a number n
 divisors :: Integral a => a -> [a]
 divisors n = concatMap f (filter ((==) 0 . mod n) (takeWhile (\k -> k*k <= n) [1 .. n]))
-    where f k | k < l = [k, l]
+    where f k | k < l     = [k, l]
               | otherwise = [k]
-              where l = div n k
+              where 
+                  l = div n k
 
 solve :: Int -> Int 
 solve index 
