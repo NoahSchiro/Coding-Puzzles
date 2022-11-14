d153a9a923c38d8a2e2d7e6b946ab4b9ae96b033
NoahSchiro <noahschiro@rocketmail.com>
2022-11-14 15:18:49 -0500

    Done with tree problems in haskell

--- haskell_99_problems/s005.hs
+++ haskell_99_problems/s005.hs
@@ -43,7 +43,67 @@ tree4 = Branch 1
 cbalTree :: Int -> [Tree Char]
 cbalTree 0 = [Empty]
 cbalTree n = let (q, r) = (n - 1) `quotRem` 2
-    in [Branch 'x' left right | i     <- [q .. q + r],
+    in [Branch 'x' left right | i     <- [q..q+r],
                                 left  <- cbalTree i,
                                 right <- cbalTree (n - i - 1)]
 
+--P56
+mirrorl2 :: Tree a -> Tree a -> Bool
+--Base cases
+mirrorl2 (Empty)  (Empty) = True
+mirrorl2 (Leaf _) (Leaf _) = True
+
+--Recurse down case
+mirrorl2 (Branch _ subA1 subA2) (Branch _ subB1 subB2) =
+    (mirrorl2 subA1 subB2) && (mirrorl2 subA2 subB1)
+
+--Generic case
+mirrorl2 _ _ = False
+
+--Just check if a root node is symmetric with itself
+symmetricl1 :: Tree a -> Bool
+symmetricl1 tree = mirrorl2 tree tree
+
+--P57
+add :: Ord a => a -> Tree a -> Tree a
+add x Empty            = Branch x Empty Empty
+add x t@(Branch y l r) = case compare x y of
+                            LT -> Branch y (add x l) r
+                            GT -> Branch y l (add x r)
+                            EQ -> t
+
+construct xs = foldl (flip add) Empty xs
+
+--P58
+symCbalTrees n = if n `mod` 2 == 0 then [] else 
+    [ Branch 'x' t (reverseTree t) | t <- cbalTree (n `div` 2)]
+
+reverseTree Empty = Empty
+reverseTree (Branch x l r) = Branch x (reverseTree r) (reverseTree l)
+
+--P59
+--I have slightly modified this so that 
+--it just produces a perfectly height balanced tree
+hbalTree :: a -> Int -> Tree a
+hbalTree val height 
+    | height == 0 = Empty
+    | height == 1 = Leaf val
+    | height > 1  = Branch val (hbalTree val (height-1))
+                               (hbalTree val (height-1))
+
+
+--P60
+hbalTreeNodes :: a -> Int -> Tree a
+hbalTreeNodes val nCount 
+    | nCount == 0 = Empty
+    | nCount == 1 = Leaf val
+    | nCount == 2 = Branch val (Leaf val) Empty
+    | nCount == 3 = Branch val (Leaf val) (Leaf val)
+
+    | mod nCount 2 /= 0 =
+        Branch val (hbalTreeNodes val (div (nCount) 2))
+                   (hbalTreeNodes val (div (nCount) 2))
+
+    | otherwise        =
+        Branch val (hbalTreeNodes val (div (nCount-2) 2))
+                   (hbalTreeNodes val (div nCount 2))
